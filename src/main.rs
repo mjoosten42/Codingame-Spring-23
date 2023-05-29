3bda2004d885215d224847314b14017c58c3b4ef
Maarten Joosten <mjoosten@student.codam.nl>
2023-05-29 16:09:26 +0200

    fk

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-use std::{collections::{VecDeque, HashMap}, fmt, hash::Hash, ops::Add, format, eprintln};
+use std::{collections::{HashMap, VecDeque}, fmt};
+use std::convert::TryInto;
 
-// https://www.redblobgames.com/grids/hexagons/
+// https://www.redblobgames.com/grids/usizeagons/
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum CellType {
+	None,
+	Egg,
+	Crystal,
+}
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 enum Player {
@@ -11,11 +19,13 @@ enum Player {
 #[derive(Copy, Clone, Debug)]
 struct Cell {
 	index: usize,
+	adj: [i32; 6],
 
 	base: Option<Player>,
-	cell_type: i32, // TODO
+	cell_type: CellType,
 	resources: usize,
 	ally_ants: usize,
+	enemy_ants: usize,
 
     ally_distance: usize,
     enemy_distance: usize,
@@ -25,15 +35,25 @@ impl Cell {
 	fn new() -> Self {
 		Self {
 			index: 0,
+			adj: [-1; 6],
 			base: None,
-			cell_type: 0,
+			cell_type: CellType::None,
 			resources: 0,
 			ally_ants: 0,
+			enemy_ants: 0,
 			ally_distance: 0,
 			enemy_distance: 0,
 		}
 	}
 }
+
+impl PartialEq for Cell {
+	fn eq(&self, other: &Self) -> bool {
+		self.index == other.index
+	}
+}
+
+impl Eq for Cell {}
 
 impl fmt::Display for Cell {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -41,75 +61,114 @@ impl fmt::Display for Cell {
 	}
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
-struct Hex {
-	q: i32,
-	r: i32,
-	s: i32,
-}
-
-impl Hex {
-	fn new() -> Self {
-		Self { q: 0, r: 0, s: 0 }
-	}
-
-	fn from(q: i32, r: i32, s: i32) -> Self {
-		Self { q, r, s }
-	}
-}
-
-impl fmt::Display for Hex {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		write!(f, "[{:>2} {:>2} {:>2}]", self.r, self.q, self.s)
-	}
-}
-
-impl Add for Hex {
-    type Output = Hex;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        Self { q: self.q + rhs.q, r: self.r + rhs.r, s: self.s + rhs.s }
-    }
-}
-
 fn main() {
 	let number_of_cells = input()[0] as usize;
-    let (mut cells, indices) = parse(number_of_cells);
+    let mut cells: HashMap<usize, Cell> = parse(number_of_cells);
     
-	let (&ally_base, &ally_base_cell) = cells.iter().find(|(_, cell)| cell.base == Some(Player::Ally)).unwrap();
-	let (&enemy_base, _) = cells.iter().find(|(_, cell)| cell.base == Some(Player::Enemy)).unwrap();
+	let base = *cells.values().find(|cell| cell.base == Some(Player::Ally)).unwrap();
+	let enemy_base = *cells.values().find(|cell| cell.base == Some(Player::Enemy)).unwrap();
 
-    flood_fill(&mut cells, &ally_base, |cell, dist| cell.ally_distance = dist);
+    flood_fill(&mut cells, &base, |cell, dist| cell.ally_distance = dist);
     flood_fill(&mut cells, &enemy_base, |cell, dist| cell.enemy_distance = dist);
 
 	loop {
 		let mut actions: String = String::from("WAIT;");
 	
-		parse_turn(&mut cells, &indices, number_of_cells);
+		parse_turn(&mut cells, number_of_cells);
 
-        let targets: Vec<&Cell> = cells
-            .values()
-            .filter(|cell| cell.ally_distance <= cell.enemy_distance && cell.resources > 0)
-            .collect();
-    
-        let dist = targets
-            .iter()
-            .min_by(|lhs, rhs| lhs.ally_distance.cmp(&rhs.ally_distance))
-            .unwrap().ally_distance;
+		let mut resources: Vec<&Cell> = Vec::new();
+		let mut crystals = 0;
+		let mut ants = 0;
+		let mut eggs = 0;
 
-        for target in targets.into_iter().filter(|cell| cell.ally_distance == dist) {
-            let strength = target.resources / (target.ally_distance + 1);
+		for cell in cells.values() {
+			if cell.resources > 0 {
+				resources.push(cell);
+			}
+		
+			if cell.cell_type == CellType::Crystal {
+				crystals += cell.resources;
+			}
 
-            actions.push_str(&format!("LINE {} {target} {strength};", ally_base_cell.index));
-        }
-    
+			if cell.cell_type == CellType::Egg {
+				eggs += cell.resources / (cell.ally_distance + 1);
+			}
+
+			ants += cell.ally_ants;
+		}
+
+		let mut path: Vec<usize> = vec![base.index];
+		let mut used: Vec<usize> = vec![base.index];
+	
+		while let Some(p) = bfs(&cells, *used.last().unwrap(), |cell| {
+			cell.resources > 0 && !used.contains(&cell.index)
+		}) {
+			if let Some(last) = path.last() {
+				used.push(*last);
+			}
+
+			path.extend(&p[1..]);
+		};
+
+		path.dedup();
+
+		let mut scores: Vec<usize> = Vec::new();
+
+		eprintln!("{path:?}");
+		for i in 1..path.len() {
+			let partial: Vec<&Cell> = path[..i].iter()
+				.map(|hex| cells.get(hex).unwrap())
+				.collect();
+
+			let richness: usize = partial.iter()
+				.map(|cell| cell.resources.clamp(0, 1))
+				.sum();
+
+			eprintln!("{richness}: {partial:?}");
+
+			scores.push(richness / i);
+		}
+
+		let best = *scores.iter().max().unwrap();
+
+		for hex in &path[0..best] {
+			actions.push_str(&format!("BEACON {hex} 1"));
+		}
+	
 		println!("{actions}");
 	}
 }
 
-fn flood_fill(cells: &mut HashMap<Hex, Cell>, start: &Hex, f: impl Fn(&mut Cell, usize)) {
-	let mut edges: Vec<Hex> = vec![*start].into();
-	let mut visited: Vec<Hex> = edges.clone();
+fn calc_paths(cells: &HashMap<usize, Cell>, base: &Cell, end: &Cell) -> Vec<Vec<usize>> {
+	let mut paths: VecDeque<Vec<usize>> = vec![vec![base.index]].into();
+	let mut answers: Vec<Vec<usize>> = Vec::new();
+
+	while !paths.is_empty() {
+		let path = paths.pop_front().unwrap();
+		let last = path.last().unwrap();
+		let cell = cells.get(last).unwrap();
+
+		if cell == end {
+			answers.push(path.clone());
+		}
+
+		if path.len() > end.ally_distance {
+			continue;
+		}
+
+		for adj in adjacent(cell) {
+			if cells.get(&adj).unwrap().ally_distance > cell.ally_distance {
+				paths.push_back(path.clone().into_iter().chain([adj]).collect());
+			}
+		}
+	}
+
+	answers
+}
+
+fn flood_fill(cells: &mut HashMap<usize, Cell>, start: &Cell, mut f: impl FnMut(&mut Cell, usize)) {
+	let mut edges: Vec<usize> = vec![start.index];
+	let mut visited: Vec<usize> = edges.clone();
 	let mut distance = 0;
 
 	while !edges.is_empty() {
@@ -120,10 +179,10 @@ fn flood_fill(cells: &mut HashMap<Hex, Cell>, start: &Hex, f: impl Fn(&mut Cell,
 
             f(cell, distance);
 		
-			for hex in adjacent(cells, &edge) {
-				if !visited.contains(&hex) {
-					adding.push(hex);
-					visited.push(hex);
+			for u in adjacent(cell) {
+				if !visited.contains(&u) {
+					adding.push(u);
+					visited.push(u);
 				}
 			}
 		}
@@ -134,117 +193,85 @@ fn flood_fill(cells: &mut HashMap<Hex, Cell>, start: &Hex, f: impl Fn(&mut Cell,
 	}
 }
 
-fn parse_turn(cells: &mut HashMap<Hex, Cell>, indices: &HashMap<usize, Hex>, n: usize) {
+fn bfs(cells: &HashMap<usize, Cell>, start: usize, f: impl Fn(&Cell) -> bool) -> Option<Vec<usize>> {
+	let mut edges: VecDeque<Vec<usize>> = vec![vec![start]].into();
+	let mut visited: Vec<usize> = Vec::new();
+
+	while !edges.is_empty() {
+		let path = edges.pop_front().unwrap();
+		let last = path.last().unwrap();
+		let cell = cells.get(last).unwrap();
+
+		if f(cell) {
+			return Some(path);
+		}
+	
+		for u in adjacent(cell) {
+			if !visited.contains(&u) {
+				edges.push_back(path.clone().into_iter().chain([u]).collect());
+			}
+		}
+
+		visited.push(*last);
+	}
+
+	None
+}
+
+fn parse_turn(cells: &mut HashMap<usize, Cell>, n: usize) {
     for i in 0..n {
+        let cell = cells.get_mut(&i).unwrap();
         let inputs = input();
-        let resources = inputs[0]; // the current amount of eggs/crystals on this cell
-        let allies = inputs[1]; // the amount of your ants on this cell
-        let _enemies = inputs[2]; // the amount of opponent ants on this cell
 
-        let hex = indices.get(&i).unwrap();
-        let cell = cells.get_mut(hex).unwrap();
-
-        cell.resources = resources as usize;
-        cell.ally_ants = allies as usize;
+		cell.resources = inputs[0] as usize;
+		cell.ally_ants = inputs[1] as usize;
+		cell.enemy_ants = inputs[2] as usize;
     }
 }
 
-fn parse(n: usize) -> (HashMap<Hex, Cell>, HashMap<usize, Hex>) {
-	let mut tmp_cells: HashMap<usize, Cell> = HashMap::new();
-	let mut indices: Vec<(Cell, [i32; 6])> = Vec::new();
+fn parse(n: usize) -> HashMap<usize, Cell> {
+	let mut cells: HashMap<usize, Cell> = HashMap::new();
 
 	for i in 0..n {
 		let mut cell = Cell::new();
 		let inputs = input();
 
 		cell.index = i;
-		cell.cell_type = inputs[0];
+		cell.cell_type = match inputs[0] {
+			0 => CellType::None,
+			1 => CellType::Egg,
+			2 => CellType::Crystal,
+			_ => panic!(),
+		};
 		cell.resources = inputs[1] as usize;
+		cell.adj = inputs[2..].try_into().unwrap();
 
-		tmp_cells.insert(i, cell);
-	
-		let mut tmp = [0; 6];
-
-		tmp.clone_from_slice(&inputs[2..]);
-
-		indices.push((cell, tmp));
+		cells.insert(i, cell);
 	}
-
-	let mut cells: HashMap<Hex, Cell> = hex_grid(&indices);
-
-    let index_map: HashMap<usize, Hex> = cells.iter().map(|(hex, cell)| (cell.index, *hex)).collect();
 
 	let _number_of_bases = input()[0];
 
 	for i in input() {
-		cells.get_mut(index_map.get(&(i as usize)).unwrap()).unwrap().base = Some(Player::Ally);
+		cells.get_mut(&(i as usize)).unwrap().base = Some(Player::Ally);
 	}
 
 	for i in input() {
-		cells.get_mut(index_map.get(&(i as usize)).unwrap()).unwrap().base = Some(Player::Enemy);
+		cells.get_mut(&(i as usize)).unwrap().base = Some(Player::Enemy);
 	}
 
-    (cells, index_map)
+    cells
 }
 
-fn hex_grid(indices: &Vec<(Cell, [i32; 6])>) -> HashMap<Hex, Cell> {
-	let zero = *indices.iter().find(|(c, _)| c.index == 0).unwrap();
-	let mut cells: HashMap<Hex, Cell> = HashMap::from([(Hex::new(), zero.0)]);
-	let mut edges: VecDeque<(Cell, [i32; 6])> = vec![zero].into();
-	let mut visited: Vec<usize> = vec![zero.0.index];
+fn adjacent(cell: &Cell) -> Vec<usize> {
+	let mut adj = Vec::new();
 
-	while !edges.is_empty() {
-		let (cell, links) = edges.pop_front().unwrap();
-
-		let adjacent: Vec<(Cell, [i32; 6])> = links
-			.iter()
-			.filter(|i| **i != -1 && !visited.contains(&(**i as usize)))
-			.map(|link| indices[*link as usize])
-			.collect();
-
-		for (c, l) in adjacent {
-			let (offset, _) = links
-				.iter()
-				.enumerate()
-				.filter(|(_, l)| **l != -1)
-				.find(|&(_, d)| *d == c.index as i32)
-				.unwrap();
-
-			let (h, _) = cells.iter().find(|(_, d)| d.index == cell.index).unwrap();
-			let hex = *h + hexes()[offset];
-
-			cells.insert(hex, c);
-			edges.push_back((c, l));
-			visited.push(c.index);
+	for a in cell.adj {
+		if a != -1 {
+			adj.push(a as usize);
 		}
 	}
 
-	cells
-}
-
-fn adjacent(cells: &HashMap<Hex, Cell>, hex: &Hex) -> Vec<Hex> {
-    let adj = hexes().map(|a| *hex + a);
-
-    let mut answer = Vec::new();
-    
-    for a in adj {
-        if cells.contains_key(&a) {
-            answer.push(a);
-        }
-    }
-
-    answer
-}
-
-fn hexes() -> [Hex; 6] {
-    [
-        Hex::from(1, 0, -1),
-        Hex::from(1, -1, 0),
-        Hex::from(0, -1, 1),
-        Hex::from(-1, 0, 1),
-        Hex::from(-1, 1, 0),
-        Hex::from(0, 1, -1),
-    ]
+	adj
 }
 
 fn input() -> Vec<i32> {
